0ee9926bb12d87e636466b01dd59baad50f775de
gopesh-pandey <30283512+gopesh-pandey@users.noreply.github.com>
2018-07-17 22:05:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
+import MySQLdb
+
 
 #Create a engine for connecting to SQLite3.
 #Assuming salaries.db is in your app root folder
 
-e = create_engine('sqlite:///salaries.db')
+#e = create_engine('sqlite:///salaries.db')
+# Connect
+db = MySQLdb.connect(host="localhost",user="appuser",passwd="",db="onco")
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,14 +18,16 @@ api = Api(app)
 class Departments_Meta(Resource):
 	def get(self):
 		#Connect to databse
-		conn = e.connect()
+		#conn = e.connect()
+		conn = db.cursor()
 		#Perform query and return JSON data
 		query = conn.execute("select distinct DEPARTMENT from salaries")
 		return {'departments': [i[0] for i in query.cursor.fetchall()]}
 
 class Departmental_Salary(Resource):
     def get(self, department_name):
-    	conn = e.connect()
+    	#conn = e.connect()
+	conn = db.cursor()
     	query = conn.execute("select * from salaries where Department='%s'"%department_name.upper())
         #Query the result and get cursor.Dumping that data to a JSON is looked by extension
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}

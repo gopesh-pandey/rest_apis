ed32a1b75e4cb5a81a837f1da9e270378d1ad462
gopesh-pandey <30283512+gopesh-pandey@users.noreply.github.com>
2018-07-19 21:46:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+from flask import Flask, jsonify
 from flask import Flask, request
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
+#from sqlalchemy import create_engine
 from json import dumps
 import MySQLdb
 
@@ -16,24 +17,31 @@ app = Flask(__name__)
 api = Api(app)
 
 class Departments_Meta(Resource):
-	def get(self):
-		#Connect to databse
-		#conn = e.connect()
-		conn = db.cursor()
-		#Perform query and return JSON data
-		query = conn.execute("select distinct DEPARTMENT from salaries")
-		return {'departments': [i[0] for i in query.cursor.fetchall()]}
+        def get(self):
+                #Connect to databse
+
+                cursor = db.cursor()
+                #Perform query and return JSON data
+                cursor.execute("select distinct product_name from products")
+                r = [dict((cursor.description[i][0], value)
+                        for i, value in enumerate(row)) for row in cursor.fetchall()]
+                return jsonify({'myCollection' : r})
+                #return {'departments': [i[0] for i in query.cursor.fetchall()]}
 
 class Departmental_Salary(Resource):
     def get(self, department_name):
-    	#conn = e.connect()
-	conn = db.cursor()
-    	query = conn.execute("select * from salaries where Department='%s'"%department_name.upper())
+        #conn = e.connect()
+        cursor = db.cursor()
+        cursor.execute("select * from products where geography='%s'"%department_name.upper())
+
+        r = [dict((cursor.description[i][0], value)
+                        for i, value in enumerate(row)) for row in cursor.fetchall()]
+        return jsonify({'myCollection' : r})
         #Query the result and get cursor.Dumping that data to a JSON is looked by extension
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return result
+        #result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        #return result
     #We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
-       
+
 api.add_resource(Departmental_Salary, '/dept/<string:department_name>')
 api.add_resource(Departments_Meta, '/departments')
 
